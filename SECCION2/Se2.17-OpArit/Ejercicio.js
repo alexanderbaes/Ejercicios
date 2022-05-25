b44a32e276fbd4d9dedfb9ba257034631d2e028c
alexanderbaes <alexanderbaes2022@gmail.com>
2022-05-25 14:59:59 -0500

    Operaciones Aritmeticas

---
+++ SECCION2/Se2.17-OpArit/Ejercicio.js
@@ -0,0 +1,12 @@
+let valor1 = 80;
+let res = 50 / 3 + (25 - valor1);
+
+document.write(res + "<br><br>");
+
+let res2 = 19 + 40 / 5 - (20 * 3);
+
+document.write(res2 + "<br><br>");
+
+let res3 = 12 + 80 * 3 - 10;
+
+document.write(res3 + "<br>");
