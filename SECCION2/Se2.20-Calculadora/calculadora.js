5ddb2c1f0fcb7f33c68afa0a329b42933ecc8423
alexanderbaes <alexanderbaes2022@gmail.com>
2022-05-25 16:39:40 -0500

    calculadora

---
+++ SECCION2/Se2.20-Calculadora/calculadora.js
@@ -0,0 +1,10 @@
+
+alert("Calculadora de Salario");
+let nomuser = prompt("Ingresa nombre de usuario: ");
+let saldia = prompt("Ingresar salario por dia: ");
+let diastra = prompt("Ingresar dias trabajados: ")
+
+let res = saldia * diastra;
+
+document.write("El Salario de " + nomuser + " es de: " + res);
+
