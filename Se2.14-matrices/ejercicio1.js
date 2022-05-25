b519ac696bf16ebc7b2e1dced2fbdcd3a5620cac
alexanderbaes <alexanderbaes2022@gmail.com>
2022-05-25 11:09:15 -0500

    seccion 2

---
+++ Se2.14-matrices/ejercicio1.js
@@ -0,0 +1,25 @@
+let paises = [
+    "Argentina",
+    "Australia",
+    "Belgica",
+    "Brasil",
+    "chile",
+    "Egipto",
+    "Francia",
+    "Panama",
+    "Sudafrica",
+    "Turquia",
+    "Tunez"
+];
+
+console.log(`el tamaño de la matriz es: ${paises.length}`);
+console.log(`el valor de la posicion 3 es: ${paises[3]}`);
+console.log(`el valor de la posicion 0 es: ${paises[0]}`);
+console.log(`el valor de la posicion -2 es: ${paises[-2]}`);
+
+console.log(`el valor de la posicion 15 es: ${paises[15]}`);
+console.log(`el valor de la posicion 8 es: ${paises[8]}`);
+console.log(`el valor de la posicion 13 es: ${paises[13]}`);
+console.log(`el valor de la posicion 4 es: ${países[4]}`); // países is not defined
+
+
