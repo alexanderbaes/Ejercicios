95b7366501ad2550579d3337bf7973efdd06d75e
alexanderbaes <alexanderbaes2022@gmail.com>
2022-05-25 17:53:45 -0500

    Operaciones CiD

---
+++ SECCION3/Se3.22-OperadoresCiD/ejercicio.js
@@ -0,0 +1,17 @@
+if(5>5){document.write("Verdadero o true <br><br>");   }else{document.write("Falso <br><br>");  }
+
+if(12<8){document.write("Verdadero o true <br><br>");   }else{document.write("Falso <br><br>");   }
+
+if("Si" == "Sí"){document.write("Verdadero o true <br><br>");   }else{document.write("Falso <br><br>");   }
+
+if('F' != '@'){document.write("Verdadero o true <br><br>");   }else{document.write("Falso <br><br>");  }
+
+if(true){document.write("Verdadero o true <br><br>");   }else{document.write("Falso <br><br>");   }
+
+if(false){ document.write("Verdadero o true <br><br>");  }else{document.write("Falso <br><br>");   }
+
+if(8 >= 8){document.write("Verdadero o true<br><br>");   }else{document.write("Falso <br><br>");   }
+
+if(30.27 <= 50){document.write("Verdadero o true <br><br>");   }else{document.write("Falso <br><br>");   }
+
+if("a" != 50){document.write("Verdadero o true <br><br>");  }else{document.write("Falso <br><br>");   }
