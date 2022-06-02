014a7b93d78fa25b8958bebbf6c88b3d063728d4
alexanderbaes <alexanderbaes2022@gmail.com>
2022-06-02 09:46:55 -0500

    ejercicio de recorrer map resuelto

--- Ejercicios Maestro/SECCION4/Se4.17RecorrerMap/codigo.js
+++ Ejercicios Maestro/SECCION4/Se4.17RecorrerMap/codigo.js
@@ -1,35 +1,53 @@
 
-const alumnos = new Map();
+let a= {
+    "santiago": 8,
+    "ana": 4,
+    "carlos": 9,
+    "ulises": 5,
+    "adan": 10
+} 
 
- alumnos.set("santiago", 8)
- alumnos.set("ana", 4)
- alumnos.set("carlos", 9)
- alumnos.set("ulises", 5)
- alumnos.set("adan", 10)
- 
+console.log(a)
 
-console.log(alumnos);
+console.log("-------------------------------") 
+console.log("Orden de entrada")
+for (let clave in a) {
+    console.log('el alumnno(Propiedad): ', clave, '=> saco (valor): =>',a[clave]);
+}
 
-const alum = [
-    {"santiago" : 8}, 
-    {"ana" : 4},
-    {"carlos" : 9},
-    {"ulises" : 5},
-    {"adan" : 10}
-];
+console.log("-------------------------------") 
+console.log("antes de ordenar por clave o por valor")
 
-const alumno = {
-    "santiago" : 8, 
-    "ana" : 4,
-    "carlos" : 9,
-    "ulises" : 5,
-    "adan" : 10
-};
+//array de arrays: [ [clave, valor], [clave, valor], ... ]
+
+let entryArray = Object.entries(a); 
+console.log(entryArray.toString());
+
+// Ordenando por clave tomando como referencia el menor
+console.log("--------------------")
+console.log("ordenado por clave")
+entryArray.sort((a,b) => a[0].localeCompare(b[0]));
+console.log(entryArray.toString());
+
+//ordenando por valor tomando como referencia el menor
+console.log("--------------------")
+console.log("ordenado por valor")
+entryArray.sort((a,b) => a[1] -b[1]);
+console.log(entryArray.toString());
 
 
+//ordenado por clave tomando como referencia el mayor
 
-console.log(Object.values(alumno));
-console.log(Object.keys(alumno));
+console.log("--------------------")
+console.log("ordenado por clave mayor")
+entryArray.sort((a,b) => a[0].localeCompare(b[0])).reverse();
+console.log(entryArray.toString());
+
+//ordenado por valor tomando como referencia el mayor
+console.log("--------------------")
+console.log("ordenado por valor mayor")
+entryArray.sort((a,b) => a[1] -b[1]).reverse();
+console.log(entryArray.toString());
 
 
 
